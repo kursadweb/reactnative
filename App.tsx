ce50deeaea66c714607ad49c98888352e91b046e
kursadweb <kursadweb@gmail.com>
2023-06-07 20:31:56 +0300

    first update App

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import { StyleSheet, SafeAreaView, Text, View} from 'react-native';
 
 const NewComponent = (props: { title: string}) => {
 
+  
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'gray'}}>
       <Text style={{fontSize:40}}>{props.title}</Text>
     </View>
   )
